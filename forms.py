5f3fa9e04927137993112dce435ca39f7d1f92e9
Ben Edwards <benedwards.dev@gmail.com>
2026-06-09 13:23:19 +1200

    Add item to pantry form

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, FloatField, DateField
 from wtforms.validators import InputRequired, Length, ValidationError, Email, EqualTo
 from models import User
 # The RegisterForm class defines the fields and validation rules for the registration form using Flask-WTF and WTForms.
@@ -22,3 +22,39 @@ class LoginForm(FlaskForm):
     email = StringField(validators=[InputRequired(), Email(message='Invalid email'), Length(max=150)])
     password = PasswordField(validators=[InputRequired(), Length(min=8, max=256)])
     submit = SubmitField('Login')
+
+class AddPantryItemForm(FlaskForm):
+    """
+    Form used to add an item to the users pantry
+    """
+
+    # Ingredient name
+    name = StringField(
+        validators=[
+            InputRequired(),
+            Length(min=1, max=100)
+        ]
+    )
+
+    # Qty Owned
+    quantity = FloatField(
+        validators=[
+            InputRequired()
+        ]
+    )
+
+    # Unit (kg, g, L, mL etc)
+    unit = StringField(
+        validators=[
+            Length(max=50)
+        ]
+    )
+
+    # Optional Exp date
+    expiry_date = DateField(
+        format="%Y-%m-%d",
+        validators=[],
+        default=None
+    )
+
+    submit = SubmitField("Add Item")
